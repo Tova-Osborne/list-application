fcdb16a74cbdbf73f20db8a19798752e947f7d85
Tova Osborne <tovaoz@gmail.com>
2022-12-02 13:43:40 -0800

    Task 1.2 - JS basics pt 1
    created array and defined three complex objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-alert('hello world!');
-
-let favoriteFood = 'burritos';
-document.write(favoriteFood);
+let artList = [painting1, painting2, painting3];
+let painting1 = {
+  title:"Mother and Child",
+  artistDisplayName:"Njideka Akunyili Crosby",
+  period:2011,
+  contempories: ["Thomas Houseago", "Mark Bradford", "Sigmar Polke"]
+  //primaryImage:
+};
+let painting2 = {
+  title:"Interaction of color",
+  artistDisplayName:"Josef Albers",
+  period:1963,
+  contempories: ["Walter Gropius", "Ludwig Mies Van Der Rohe", "Josef Matthias Hauer"]
+  //primaryImage:
+};
+let painting3 = {
+  title:"Repose",
+  artistDisplayName:"John White Alexander",
+  period:1895,
+  contempories: ["Thomas Wilmer Dewing", "Cecilia Beaux", "Thomas Birch"]
+  //primaryImage:
+};
