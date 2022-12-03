ed90870c0d06ead9d5a8b48ae4eeaa5ef96fe2b2
Tova Osborne <tovaoz@gmail.com>
2022-12-02 19:51:43 -0800

    Task - 1.3: JavaScript Basics Part 2
    Updated the array to remove the variables "painting1"... Created a for loop and conditional to categorize by painting period.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,50 @@
-let artList = [painting1, painting2, painting3];
+let artList = [{
+    title: "Mother and Child",
+    artistDisplayName: "Njideka Akunyili Crosby",
+    period: 2011,
+    contempories: ["Thomas Houseago", "Mark Bradford", "Sigmar Polke"]
+    //primaryImage:
+  },
+  {
+    title: "Interaction of color",
+    artistDisplayName: "Josef Albers",
+    period: 1963,
+    contempories: ["Walter Gropius", "Ludwig Mies Van Der Rohe", "Josef Matthias Hauer"]
+    //primaryImage:
+  },
+  {
+    title: "Repose",
+    artistDisplayName: "John White Alexander",
+    period: 1895,
+    contempories: ["Thomas Wilmer Dewing", "Cecilia Beaux", "Thomas Birch"]
+    //primaryImage:
+  }
+];
+
+
+for (let i = 0; i < artList.length; i++) {
+  if (artList[i].period < 1900) {
+    document.write(`Paintings from the 19th Century:
+      ${artList[i].title}
+      ${artList[i].artistDisplayName}
+      ${artList[i].period}`)
+  } else if (artList[i].period > 1900 && artList[i].period < 2000) {
+    document.write(`Paintings from the 20th Century:
+      ${artList[i].title}
+      ${artList[i].artistDisplayName}
+      ${artList[i].period}`)
+  } else
+    document.write(`Paintings from the 21st Century:
+      ${artList[i].title}
+      ${artList[i].artistDisplayName}
+      ${artList[i].period}`)
+}
+
+
+
+
+/*OLD ARRAY
+
 let painting1 = {
   title:"Mother and Child",
   artistDisplayName:"Njideka Akunyili Crosby",
@@ -20,3 +66,6 @@ let painting3 = {
   contempories: ["Thomas Wilmer Dewing", "Cecilia Beaux", "Thomas Birch"]
   //primaryImage:
 };
+
+let artList = [painting1, painting2, painting3];
+*/
